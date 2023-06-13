6aaf32bfe17de3316320643c23ccfc457a8ec404
samagomenne@gmail.com <samagomenne@gmail.com>
2023-06-13 19:21:40 +0700

    login siswa dan style

--- resources/views/login/login-siswa.blade.php
+++ resources/views/login/login-siswa.blade.php
@@ -34,58 +34,40 @@
 <body>
 
   <main>
-    <div class="container">
+      <div class="container-fluid login-custom">
 
-      <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
-        <div class="container">
-          <div class="row justify-content-center">
-            <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
-
-              <div class="d-flex justify-content-center py-4">
-                <a href="index.html" class="logo d-flex align-items-center w-auto">
-                  <img src="assets/img/lg.png" alt="">
-                  <span class="d-none d-lg-block">E-SCHOOL</span>
-                </a>
-              </div><!-- End Logo -->
-
-              <div class="card mb-3">
-
-                <div class="card-body">
-
-                  <div class="pt-4 pb-2">
-                    <h5 class="card-title text-center pb-0 fs-4">Masuk Menggunakan Akun Anda</h5>
-                    <p class="text-center small">Enter your username & password to login</p>
-                  </div>
-
-                  <form class="row g-3 needs-validation" novalidate>
-
-                    <div class="col-12">
-                      <label for="yourUsername" class="form-label">Username</label>
-                      <div class="input-group has-validation">
-                        <span class="input-group-text" id="inputGroupPrepend">@</span>
-                        <input type="text" name="username" class="form-control" id="yourUsername" required>
-                        <div class="invalid-feedback">Please enter your username.</div>
-                      </div>
-                    </div>
-
-                    <div class="col-12">
-                      <label for="yourPassword" class="form-label">Password</label>
-                      <input type="password" name="password" class="form-control" id="yourPassword" required>
-                      <div class="invalid-feedback">Please enter your password!</div>
-                    </div>
-                    <div class="col-12">
-                      <button class="btn btn-primary w-100" type="submit">Login</button>
-                    </div>
-                  </form>
-
-                </div>
-              </div>
-
-            </div>
+      <section class="vh-100 container">
+        <div class="container-fluid h-custom">
+          <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col-md-9 col-lg-6 col-xl-5">
+            <img src="img/lg.png"
+            class="img-fluid" alt="Sample image" width="400px">
           </div>
-        </div>
+        <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+        <form>
+          <!-- Email input -->
+          <div class="form-outline mb-4">
+            <label class="form-label" for="form3Example3">Username</label>
+            <input type="text" id="form3Example3" class="form-control form-control-lg"
+              placeholder="Masukkan username anda" />
+          </div>
 
-      </section>
+          <!-- Password input -->
+          <div class="form-outline mb-3">
+            <label class="form-label" for="form3Example4">Password</label>
+            <input type="password" id="form3Example4" class="form-control form-control-lg"
+              placeholder="Enter password" />
+          </div>
+
+          <div class="text-center text-lg-start mt-4 pt-2">
+            <button type="button" class="btn btn-lg">Login</button>
+          </div>
+
+        </form>
+      </div>
+    </div>
+  </div>
+</section>
 
     </div>
   </main><!-- End #main -->
